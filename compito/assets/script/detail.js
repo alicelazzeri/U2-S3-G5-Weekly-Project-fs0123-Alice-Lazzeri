da039b7f6c084e651288142113d069cb51536c86
alicelazzeri <alice.lazzeri@hotmail.it>
2023-03-19 18:05:41 +0100

    added hover

--- compito/assets/script/detail.js
+++ compito/assets/script/detail.js
@@ -29,15 +29,15 @@ const createCard = (name, brand, imgUrl, price, description) => {
   col.setAttribute("class", "col");
   row.appendChild(col);
 
-  col.innerHTML = `<div id="detail" class="card d-flex flex-row align-items-center px-3 py-4 my-5 border border-2 border-warning-subtle rounded-3">
-    <img class="img-fluid card-img-top p-3" style="object-fit: contain; object-position: top; height: 350px;" src="${imgUrl}" alt="Card pic" />
+  col.innerHTML = `<div id="detail" class="card d-flex flex-column flex-sm-column flex-md-column flex-lg-row align-items-center px-3 py-4 my-5 border border-2 border-warning-subtle rounded-3 shadow-lg">
+    <img class="img-fluid card-img-top p-3" style="object-fit: contain; object-position: top; height: 350px; border-radius: 20px;" src="${imgUrl}" alt="Card pic" />
     <div class="d-flex flex-column">
     <div class="card-body text-start px-3">
-      <h5 class="card-title text-start display-5 px-3 mb-4" style="font-family: 'Sansita Swashed', cursive">${name}</h5>
-      <h4 class="card-text text-start fw-light px-3">${brand}</h4>
-      <p class="card-text text-start fw-lighter px-3">${price}€</p>
-      <p class="card-text text-start fw-light px-3">${description}</p>
+      <h5 class="card-title text-center text-sm-center text-md-center text-lg-start display-5 px-3 mb-4" style="font-family: 'Sansita Swashed', cursive">${name}</h5>
+      <h4 class="card-text text-center text-sm-center text-md-center text-lg-start fw-lighter px-3 mb-4" style="font-family: 'Sansita Swashed', cursive">${brand}</h4>
+      <p class="card-text text-center text-sm-center text-md-center text-lg-start fw-lighter px-3">${price}€</p>
+      <p class="card-text text-center text-sm-center text-md-center text-lg-start fw-light px-3 mb-4">${description}</p>
     </div>
-    <a class="btn btn-outline-warning border-1 w-75 mx-auto" href="index.html" role="button">Back to Homepage</a>
+    <a class="btn btn-outline-warning border-1 w-75 mx-auto mb-3 mb-sm-3" href="index.html" role="button">Back to Homepage</a>
   </div>`;
 };
